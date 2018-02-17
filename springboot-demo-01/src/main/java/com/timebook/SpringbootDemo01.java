0abb0221b6de5ea65ac96e6e59607e146475d235
zelin <zelin.xiong@ctgtmo.com>
2018-02-17 15:01:35 +0800

    Initial commit

---
+++ springboot-demo-01/src/main/java/com/timebook/SpringbootDemo01.java
@@ -0,0 +1,18 @@
+package com.timebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @title: SpringbootDemo01
+* @author: xiongzl
+* @date: 2018/2/17
+* @descprition: 
+*/
+@SpringBootApplication
+public class SpringbootDemo01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo01.class, args);
+	}
+}
