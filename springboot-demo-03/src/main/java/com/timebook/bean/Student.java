142615f5193fb12f22becda4c5a3adf1bbda5dbd
zelin <zelin.xiong@ctgtmo.com>
2018-02-20 22:02:42 +0800

    1.other

--- springboot-demo-03/src/main/java/com/timebook/bean/Student.java
+++ springboot-demo-03/src/main/java/com/timebook/bean/Student.java
@@ -10,7 +10,7 @@ import java.util.Date;
  * @date: 2018/2/17
  * @descprition: Student对象
  */
-@Data
+
 public class Student {
 
     private long id;
@@ -22,4 +22,52 @@ public class Student {
     private int sex;
 
     private Date createTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public Date getCreateTime() {
+        if (null == createTime) {
+            return null;
+        } else {
+            return (Date) createTime.clone();
+        }
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public void setCreateTime(Date createTime) {
+        if (null == createTime) {
+            this.createTime = null;
+        } else {
+            this.createTime = (Date)createTime.clone();
+        }
+    }
 }
