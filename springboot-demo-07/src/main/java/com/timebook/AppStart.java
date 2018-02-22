ee6403909349928b72cd39c2fa59797165b7a869
zelin <zelin.xiong@ctgtmo.com>
2018-02-22 09:36:16 +0800

    1. springboot-demo-07 merge

---
+++ springboot-demo-07/src/main/java/com/timebook/AppStart.java
@@ -0,0 +1,30 @@
+package com.timebook;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * The type App start.
+ */
+@SpringBootApplication
+public class AppStart {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AppStart.class, args);
+	}
+
+
+
+
+}
