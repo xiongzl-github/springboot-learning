2db814fd8fe819f74b6de2f6531e379b1385616e
zelin <zelin.xiong@ctgtmo.com>
2018-02-23 12:37:46 +0800

    1. merge springboot-demo-09

---
+++ springboot-demo-09/src/main/java/com/timebook/AppStart.java
@@ -0,0 +1,22 @@
+package com.timebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type App start.
+ *
+ * @author xiongzl
+ */
+@SpringBootApplication
+public class AppStart {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AppStart.class, args);
+	}
+}
